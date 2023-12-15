f83cd73d94cfd9b928329ce018624299fe4750dc
Demon Martin <49092993+DemonMartin@users.noreply.github.com>
2023-12-15 13:59:37 +0100

    fix timer maybe

--- main.js
+++ main.js
@@ -5,8 +5,10 @@ let volume = 0.0;
 
 let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 let microphoneStream = null;
-let analyserNode = audioCtx.createAnalyser()
+let analyserNode = audioCtx.createAnalyser();
 const frequencyDisplayElement = document.querySelector('#frequency');
+
+let timerInterval;
 
 const data = {
     minti: {
@@ -31,75 +33,15 @@ const data = {
     },
 };
 
-
-
-
-function createAudioMeter(audioContext, clipLevel, averaging, clipLag) {
-    var processor = audioContext.createScriptProcessor(512);
-    processor.onaudioprocess = volumeAudioProcess;
-    processor.clipping = false;
-    processor.lastClip = 0;
-    processor.volume = 0;
-    processor.clipLevel = clipLevel || 0.98;
-    processor.averaging = averaging || 0.95;
-    processor.clipLag = clipLag || 750;
-
-    // this will have no effect, since we don't copy the input to the output,
-    // but works around a current Chrome bug.
-    processor.connect(audioContext.destination);
-
-    processor.checkClipping =
-        function () {
-            if (!this.clipping)
-                return false;
-            if ((this.lastClip + this.clipLag) < window.performance.now())
-                this.clipping = false;
-            return this.clipping;
-        };
-
-    processor.shutdown =
-        function () {
-            this.disconnect();
-            this.onaudioprocess = null;
-        };
-
-    return processor;
-}
-
-function volumeAudioProcess(event) {
-    var buf = event.inputBuffer.getChannelData(0);
-    var bufLength = buf.length;
-    var sum = 0;
-    var x;
-
-    // Do a root-mean-square on the samples: sum up the squares...
-    for (var i = 0; i < bufLength; i++) {
-        x = buf[i];
-        if (Math.abs(x) >= this.clipLevel) {
-            this.clipping = true;
-            this.lastClip = window.performance.now();
-        }
-        sum += x * x;
-    }
-
-    // ... then take the square root of the sum.
-    var rms = Math.sqrt(sum / bufLength);
-
-    // Now smooth this out with the averaging factor applied
-    // to the previous sample - take the max here because we
-    // want "fast attack, slow release."
-    this.volume = Math.max(rms, this.volume * this.averaging);
-}
+// ... [Existing code for createAudioMeter and volumeAudioProcess] ...
 
 function startPitchDetection() {
-    navigator.mediaDevices.getUserMedia({
-        audio: true
-    })
+    navigator.mediaDevices.getUserMedia({ audio: true })
         .then((stream) => {
             microphoneStream = audioCtx.createMediaStreamSource(stream);
             microphoneStream.connect(analyserNode);
 
-            meter = createAudioMeter(audioCtx);
+            let meter = createAudioMeter(audioCtx);
             microphoneStream.connect(meter);
 
             im = setInterval(() => {
@@ -111,29 +53,18 @@ function startPitchDetection() {
         });
 }
 
-function restartTimer(character) {
-    data[character].mill = 0;
-    data[character].sec = 0;
-    data[character].min = 0;
-}
-
 function startTimer(character) {
     clearInterval(timerInterval);
-    data[character].mill = 0;
-    data[character].sec = 0;
-    data[character].min = 0;
-    startTime = Date.now();
-
     timerInterval = setInterval(() => {
         updateTimer(character);
     }, 10);
 }
 
 function updateTimer(character) {
-    const elapsedTime = Date.now() - startTime;
-    data[character].min = Math.floor(elapsedTime / 60000);
-    data[character].sec = Math.floor((elapsedTime % 60000) / 1000);
-    data[character].mill = Math.floor((elapsedTime % 1000) / 10);
+    const now = new Date();
+    data[character].min = now.getMinutes();
+    data[character].sec = now.getSeconds();
+    data[character].mill = Math.floor(now.getMilliseconds() / 10);
 
     text[data[character].id].innerText = formatTime(character);
 }
@@ -169,17 +100,17 @@ function rise(character) {
 
 function createBubble(position, size, speed, rlspeed, high, difference) {
     var b = document.createElement("div")
-    b.classList = "bubble"
-    b.style.left = position
-    b.style.width = size + "vw"
-    b.style.height = size + "vw"
-    b.animate({ opacity: ["0.7", "0"], bottom: [(-size + "vw"), high] }, { duration: speed })
-    b.animate({ transform: ["translateX(-" + difference + ")", "translateX(" + difference + ")"] }, { duration: rlspeed, iterations: Infinity, direction: "alternate", easing: "cubic-bezier(0.5, 0, 0.5, 1)" })
+    b.classList = "bubble";
+    b.style.left = position;
+    b.style.width = size + "vw";
+    b.style.height = size + "vw";
+    b.animate({ opacity: ["0.7", "0"], bottom: [(-size + "vw"), high] }, { duration: speed });
+    b.animate({ transform: ["translateX(-" + difference + ")", "translateX(" + difference + ")"] }, { duration: rlspeed, iterations: Infinity, direction: "alternate", easing: "cubic-bezier(0.5, 0, 0.5, 1)" });
 
-    document.getElementById("bubbles").appendChild(b)
+    document.getElementById("bubbles").appendChild(b);
 
     setTimeout(function () {
-        b.remove()
+        b.remove();
     }, (speed - 100));
 }
 
